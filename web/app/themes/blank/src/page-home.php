540c500369491c669f2bbede2e0c4f5a5c370fd0
Jonathan Rauzy <jr@BIGSMILE.local>
2024-02-07 12:30:27 +0100

    base.sccs

--- web/app/themes/blank/src/page-home.php
+++ web/app/themes/blank/src/page-home.php
@@ -7,20 +7,20 @@
 
 <?php get_header(); ?>
 
-<main class="default" style="background-image: url(<?php the_field('bg_img'); ?>);">
+<main class="home" style="background-image: url(<?php the_field('bg_img'); ?>);">
 	<section class="center page">
 		<div class="inner" >
 			<h2>One platform to share all your digital campaigns</h2>
 			<div class="content">
+
 				<?php the_content(); ?>
 				<?= get_field('catchphrase') ?>	
 				<?= get_field('sub_catchphrase') ?>		
 
-				<div class="grid_2">
-					<button><?= get_field('trial_btn')?></button>
-					<button><?= get_field('demo_btn')?></button>
+				<div class="grid_btn_2">					
+					<button class="solid"><a href="http://hulk-landing.local/login/"><?= get_field('trial_btn')?></a></button>
+					<button class="hollow"><a href="http://hulk-landing.local/demo/"><?= get_field('demo_btn')?></button></a>						
 				</div>
-
 			</div>
 		</div>
 	</section>
