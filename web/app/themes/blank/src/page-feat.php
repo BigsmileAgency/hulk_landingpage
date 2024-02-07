1b8cfb99a2053bd7d43a5aa1dca2e59ce31a7ce0
Gauthier <gauthier@bigsmile.be>
2024-02-07 11:27:50 +0100

    push ebauche feat

--- web/app/themes/blank/src/page-feat.php
+++ web/app/themes/blank/src/page-feat.php
@@ -9,20 +9,41 @@
 <main class="default">
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-  <?php get_template_part('components/home/header', null, array(
-      'test' => 'coucou'
-    )); ?>
+  <header class="header">
+    <h1 class="title"><?= get_field('title_header') ?></h1>
+    <p class="title_content"><?= get_field('content_header') ?></p>      
+  </header>
+  <div class="features">
+    <?php if( have_rows('features') ):
 
-     <section class="center page">
-      <div class="inner">
-        
-      <h2><?= the_title() ?></h2>
-        <div class="content">
-          <?php the_content(); ?>
+  // Loop through rows.
+  while( have_rows('features') ) : the_row();
+
+      // Load sub field value.
+      $img_feature = get_sub_field('img_feature');
+      $title_feature = get_sub_field('title_feature');
+      $content_feature = get_sub_field('content_feature'); 
+      // Do something, but make sure you escape the value if outputting directly... ?>
+      <div class="feature">
+        <div class="feature_img">
+          <img src="<?=$img_feature?>" width="375">
+        </div>
+        <div class="feature_title">
+          <?= $title_feature ?>
+        </div>
+        <div class="feature_content">
+          <?= $content_feature ?>
         </div>
       </div>
-    </section>
 
+  <?php // End loop.
+  endwhile;
+
+  // No value.
+  else :
+  // Do something...
+  endif; ?>
+  </div>
   <?php endwhile; ?>
   <?php else: ?>
     <article>
