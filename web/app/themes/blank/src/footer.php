f27984665c4a080a9bbf678bc7c5b14f428919d2
Gauthier <gauthier@bigsmile.be>
2024-02-07 16:49:08 +0100

    page feat + footer

--- web/app/themes/blank/src/footer.php
+++ web/app/themes/blank/src/footer.php
@@ -1,15 +1,45 @@
-
-    <footer class="footer">
-        <div class="inner">
-            <?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+<footer class="footer">
+    <div class="inner">
+        <?php wp_nav_menu(array('theme_location' => 'footer')); ?>
+    </div>
+    <section class="bg_footer">
+        <div class="container container_footer">
+            <div class="menu1">
+                <span class="bold">Ressources</span>
+                <ul>
+                    <li>Status page</li>
+                    <li>Social and mockups</li>
+                    <li>API</li>
+                    <li>Blog</li>
+                </ul>
+            </div>
+            <div class="menu2">
+                <span class="bold">Support</span>
+                <ul>
+                    <li>Knowledgebase</li>
+                    <li>Security</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
         </div>
-    </footer>
+    </section>
+
+    <section>
+    <div class="legal_footer">
+
+    © 2023 Big Smile Agency - Terms and conditions | Privacy policy | Cookie policy
+    </div>
+</section>
 
 
-    <script>
-        const baseUrl = '<?= WP_HOME ?>';
-    </script>
-    
-        <?php wp_footer(); ?>
-    </body>
+</footer>
+
+
+<script>
+    const baseUrl = '<?= WP_HOME ?>';
+</script>
+
+<?php wp_footer(); ?>
+</body>
+
 </html>
