ce4a8d4e2b64330a3daeedf7a34b638eaa186a71
Jonathan Rauzy <jr@BIGSMILE.local>
2024-02-09 14:39:37 +0100

    calendar

--- web/app/themes/blank/src/components/functions/demo_form_ajax.php
+++ web/app/themes/blank/src/components/functions/demo_form_ajax.php
@@ -3,102 +3,89 @@
 function demo_form_ajax()
 {
 ?>
-    <script>
-        let lang = document.documentElement.lang;
+	<script>
+		let lang = document.documentElement.lang;
 
-        if (lang == "fr-FR") {
-            lang = "fr";
-        }
+		if (lang == "fr-FR") {
+			lang = "fr";
+		}
 
-        let copy = {
+		let copy = {
+			emptyFields: {
+				"en": "Fields marked with * are mandatory",
+				"fr": "Les champs marqué d'un * sont obligatoire",
+			},
 
-            emptyFields: {
-                "en": "Fields marked with * are mandatory",
-                "fr": "Les champs marqué d'un * sont obligatoire",
-            },
+			badMail: {
+				"en": "Put a proper e-mail adress",
+				"fr": "Adresse e-mail non conforme",
 
-            badMail: {
-                "en": "Put a proper e-mail adress",
-                "fr": "Adresse e-mail non conforme",
+			},
 
-            },
+			badPhone: {
+				"en": "Put a proper phone number",
+				"fr": "Numéro de téléphone non-valide",
+			}
+		}
 
-            badPhone: {
-                "en": "Put a proper phone number",
-                "fr": "Numéro de téléphone non-valide",
-            }
+		document.addEventListener("DOMContentLoaded", function() {
 
-        }
+			let demoForm = document.querySelector("#demo_form")
 
-        document.addEventListener("DOMContentLoaded", function() {
+			demoForm.addEventListener("submit", (e) => {
 
-            let demoForm = document.querySelector("#demo_form")
+				e.preventDefault();
 
-            demoForm.addEventListener("submit", (e) => {
-
-                e.preventDefault();
-
-                // containers
-                let formAndCalendar = document.querySelector('.form_and_calendar');
-                let demoFormContainer = document.querySelector('.demo_form_container');
-                let calendar = document.querySelector('.calendar_container');
-                let gif = document.querySelector('.demo_gif');
+				// containers
+				let formAndCalendar = document.querySelector('.form_and_calendar');
+				let demoFormContainer = document.querySelector('.demo_form_container');
+				let calendar = document.querySelector('.calendar_container');
+				let gif = document.querySelector('.demo_gif');
 
 
-                // form input
-                let firstName = document.querySelector("#demo_first_name").value;
-                let lastName = document.querySelector("#demo_last_name").value;
-                let email = document.querySelector("#demo_email").value;
-                let phone = document.querySelector("#demo_phone").value;
-                let companyName = document.querySelector("#demo_company_name").value;
-                let isAgency = document.querySelector("#are_you_agency").checked;
-                let isConsent = document.querySelector("#demo_consent").checked;
-                let sentBtn = document.querySelector("#demo_send_btn");
-
-                let daysArray = Array.from(document.querySelectorAll('.days div'))
-
-                // response
-                let response = document.querySelector('#demo_response');
-
-                // regexs 
-                let mailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                let phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
-
-                if (firstName === "" || lastName === "" || email === "" || phone === "") {
-                    // alert(copy.emptyFields[lang]);
-                    response.textContent = copy.emptyFields[lang];
-
-                } else if (!email.match(mailRegex)) {
-                    response.textContent = copy.badMail[lang];
-                } else if (!phone.match(phoneRegex)) {
-                    response.textContent = copy.badPhone[lang];
-                } else {
-                    demoFormContainer.style.display = "none";
-
-                    gif.style.display = "block";
-
-                    setTimeout(() => {
-                        gif.style.display = "none"
-                        calendar.style.display = "block"
-                    }, 1000)
-
-                }
-            })
+				// form input
+				let firstName = document.querySelector("#demo_first_name").value;
+				let lastName = document.querySelector("#demo_last_name").value;
+				let email = document.querySelector("#demo_email").value;
+				let phone = document.querySelector("#demo_phone").value;
+				let companyName = document.querySelector("#demo_company_name").value;
+				let isAgency = document.querySelector("#are_you_agency").checked;
+				let isConsent = document.querySelector("#demo_consent").checked;
+				let sentBtn = document.querySelector("#demo_send_btn");
+				let calendarContainer = document.querySelector('.calendar_container');
 
 
-            let calendar = document.querySelector('.calendar_container');
-            let daysArray = Array.from(document.querySelectorAll('.day'));
+				let daysArray = Array.from(document.querySelectorAll('.days div'))
 
-            console.log(calendar);
+				// response
+				let response = document.querySelector('#demo_response');
 
-            daysArray.forEach((day) => {
-                day.addEventListener('click', (e) => {
-                    console.log(e.currentTarget.dataset.date);
-                })
-            })
+				// regexs 
+				let mailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+				let phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
 
-        })
-    </script>
+				if (firstName === "" || lastName === "" || email === "" || phone === "") {
+					// alert(copy.emptyFields[lang]);
+					response.textContent = copy.emptyFields[lang];
+
+				} else if (!email.match(mailRegex)) {
+					response.textContent = copy.badMail[lang];
+				} else if (!phone.match(phoneRegex)) {
+					response.textContent = copy.badPhone[lang];
+				} else {
+					demoFormContainer.style.display = "none";
+
+					gif.style.display = "block";
+
+					setTimeout(() => {
+						gif.style.display = "none"
+						calendarContainer.style.display = "block"
+					}, 1000)
+
+				}
+			})
+		})
+	</script>
 <?php
 }
 add_action('wp_head', 'demo_form_ajax');
--- web/app/themes/blank/src/components/layouts/calendar.php
+++ web/app/themes/blank/src/components/layouts/calendar.php
@@ -1,114 +1,121 @@
 <div class="calendar">
+	<div class="month">
+		<i class="fas fa-angle-left prev"></i>
+		<div class="date">
+			<p class="month_display"></p>
+			<p class="full_date"></p>
+		</div>
+		<i class="fas fa-angle-right next"></i>
+	</div>
 
-   <div class="month">
-      <i class="fas fa-angle-left prev"></i>
-      <div class="date">
-         <p class="month_display"></p>
-         <p class="full_date"></p>
-      </div>
-      <i class="fas fa-angle-right next"></i>
-   </div>
+	<div class="weekdays">
+		<div>M</div>
+		<div>Tue</div>
+		<div>W</div>
+		<div>Thu</div>
+		<div>F</div>
+		<div>S</div>
+		<div>Sun</div>
+	</div>
 
-   <div class="weekdays">
-      <div>M</div>
-      <div>Tue</div>
-      <div>W</div>
-      <div>Thu</div>
-      <div>F</div>
-      <div>S</div>
-      <div>Sun</div>
-   </div>
-
-   <div class="days">
-   </div>
+	<div class="days">
+	</div>
 
 </div>
 
 <script>
-   document.addEventListener("DOMContentLoaded", function() {
+	const date = new Date();
 
-      const date = new Date();
+	const renderCalendar = () => {
 
-      const renderCalendar = () => {
+		date.setDate(1);
 
-         date.setDate(1);
+		const monthDays = document.querySelector(".days");
 
-         const monthDays = document.querySelector(".days");
+		const lastDay = new Date(
+			date.getFullYear(),
+			date.getMonth() + 1,
+			0
+		).getDate();
 
-         const lastDay = new Date(
-            date.getFullYear(),
-            date.getMonth() + 1,
-            0
-         ).getDate();
+		const prevLastDay = new Date(
+			date.getFullYear(),
+			date.getMonth(),
+			0
+		).getDate();
 
-         const prevLastDay = new Date(
-            date.getFullYear(),
-            date.getMonth(),
-            0
-         ).getDate();
+		const firstDayIndex = date.getDay() - 1;
 
-         const firstDayIndex = date.getDay();
+		const lastDayIndex = new Date(
+			date.getFullYear(),
+			date.getMonth() + 1,
+			0
+		).getDay();
 
-         const lastDayIndex = new Date(
-            date.getFullYear(),
-            date.getMonth() + 1,
-            0
-         ).getDay();
+		const nextDays = 7 - lastDayIndex;
 
-         const nextDays = 7 - lastDayIndex - 1;
+		const months = [
+			"January",
+			"February",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+			"August",
+			"September",
+			"October",
+			"November",
+			"December",
+		];
 
-         const months = [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December",
-         ];
+		document.querySelector(".month_display").innerHTML = months[date.getMonth()];
 
-         document.querySelector(".month_display").innerHTML = months[date.getMonth()];
+		document.querySelector(".full_date").innerHTML = new Date().toDateString();
 
-         document.querySelector(".full_date").innerHTML = new Date().toDateString();
+		let days = "";
 
-         let days = "";
+		for (let x = firstDayIndex; x > 0; x--) {
+			days += `<div class="day prev_date">${prevLastDay - x + 1}</div>`;
+		}
 
-         for (let x = firstDayIndex; x > 0; x--) {
-            days += `<div class="day prev_date">${prevLastDay - x + 1}</div>`;
-         }
+		for (let i = 1; i <= lastDay; i++) {
+			if (
+				i === new Date().getDate() &&
+				date.getMonth() === new Date().getMonth()
+			) {
+				days += `<div class="day today">${i}</div>`;
+			} else {
+				days += `<div class="day">${i}</div>`;
+			}
+		}
 
-         for (let i = 1; i <= lastDay; i++) {
-            if (
-               i === new Date().getDate() &&
-               date.getMonth() === new Date().getMonth()
-            ) {
-               days += `<div class="day today">${i}</div>`;
-            } else {
-               days += `<div class="day">${i}</div>`;
-            }
-         }
+		for (let j = 1; j <= nextDays; j++) {
+			days += `<div class="day next_date">${j}</div>`;
+			monthDays.innerHTML = days;
+		}
 
-         for (let j = 1; j <= nextDays; j++) {
-            days += `<div class="day next_date">${j}</div>`;
-            monthDays.innerHTML = days;
-         }
-      };
+		let calendar = document.querySelector('.calendar')
+		let daysArray = Array.from(calendar.querySelectorAll('.day'));
 
-      document.querySelector(".prev").addEventListener("click", () => {
-         date.setMonth(date.getMonth() - 1);
-         renderCalendar();
-      });
+		console.log(daysArray);
 
-      document.querySelector(".next").addEventListener("click", () => {
-         date.setMonth(date.getMonth() + 1);
-         renderCalendar();
-      });
+		daysArray.forEach((day) => {
+			day.addEventListener('click', (e) => {
+				console.log(e.target.innerHTML);
+			})
+		})
+	};
 
-      renderCalendar();
-   })
+	document.querySelector(".prev").addEventListener("click", () => {
+		date.setMonth(date.getMonth() - 1);
+		renderCalendar();
+	});
+
+	document.querySelector(".next").addEventListener("click", () => {
+		date.setMonth(date.getMonth() + 1);
+		renderCalendar();
+	});
+
+	renderCalendar();
 </script>
