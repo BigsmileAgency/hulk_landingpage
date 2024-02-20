e49c25b8d9aa2e8676b623bbf0d660d2291a77a5
Jonathan Rauzy <jr@BIGSMILE.local>
2024-02-20 09:49:34 +0100

    correction bugs

--- web/app/themes/blank/src/components/functions/get_all_appointements.php
+++ web/app/themes/blank/src/components/functions/get_all_appointements.php
@@ -7,15 +7,20 @@ function get_all_appointements()
 
   $appointements =
     $wpdb->get_results(
-        "SELECT * FROM wp_demo_appointement 
+      "SELECT * FROM wp_demo_appointement 
         INNER JOIN wp_time_slot 
         ON wp_demo_appointement.time_slot_id = wp_time_slot.id
         WHERE wp_demo_appointement.first_name != 'BSA'
-        ORDER BY wp_demo_appointement.date, wp_time_slot.time"      
+        AND DATE(wp_demo_appointement.date) >= DATE(NOW())
+        ORDER BY wp_demo_appointement.date, wp_time_slot.time"
     );
 
 
-  $response = $appointements;
+  if ($appointements) {
+    $response = $appointements;
+  } else {
+    $response = "Get all apointements is failed!";
+  }
 
   echo json_encode($response);
   wp_die();
