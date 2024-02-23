b9af76930a979523cc07b4be1af66661546d1a60
Jonathan Rauzy <jr@BIGSMILE.local>
2024-02-23 16:46:19 +0100

    new design v1.0

--- web/app/themes/blank/src/footer.php
+++ web/app/themes/blank/src/footer.php
@@ -25,7 +25,7 @@
                 </div>
                 
             </div>
-            <div class="logo_hulk"><img class="logo_hulk" src="<?php echo get_template_directory_uri() ?>/images/HulkBanner.png"></div>
+            <div class="logo_hulk"><img class="logo_hulk" src="<?php echo get_template_directory_uri() ?>/images/logo_footer.svg"></div>
         </div>
     </section>
 
--- web/app/themes/blank/src/header.php
+++ web/app/themes/blank/src/header.php
@@ -35,10 +35,9 @@
 </head>
 
 <body>
-
     <header class="header">
         <div class=" container container_nav">
-            <div class="logo_nav"><a href="<?php echo get_home_url(); ?>"><img class="logo_hulk" src="<?php echo get_template_directory_uri() ?>/images/HulkBanner2.png"></a></div>
+            <div class="logo_nav"><a href="<?php echo get_home_url(); ?>"><img class="logo_hulk" src="<?php echo get_template_directory_uri() ?>/images/logo_header.svg"></a></div>
             <?php wp_nav_menu(array('theme_location' => 'nav_menu', 'container_class' => 'nav_menu')); ?>
             <div class="nav_menu">
                 <ul id="menu-login">
