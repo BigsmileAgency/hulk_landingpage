911bb3811e078d53ff6d40c25d632070d2f2dbd3
Jonathan Rauzy <jr@BIGSMILE.local>
2024-03-12 14:48:53 +0100

    disabled anim for mobile

--- web/app/themes/blank/src/components/functions/homepage_scroll_animation.php
+++ web/app/themes/blank/src/components/functions/homepage_scroll_animation.php
@@ -7,48 +7,52 @@ function homepage_scroll_animation()
 			e.preventDefault();
 
 
-			// img_shadow on scroll
-			let boxes = gsap.utils.toArray('.img_shadow');
-			boxes.forEach((box, index) => {
-				let distance = index % 2 === 0 ? '-100%' : '100%';
-				let rotation = index % 2 === 0 ? '45' : '-45';
-				gsap.fromTo(box, {
-					x: distance,
-					yPercent: 50, 
-					rotation: 0,
-					opacity: 0,
-				}, {
-					x: 0,
-					yPercent: 0,
-					rotation: rotation,
-					duration: 2,
-					opacity: 1,
-					ease: 'expo.out',
-					scrollTrigger: {
-						trigger: box,
-						start: '-=200px center',
-						end: 'bottom 20%',
-						// scrub: .5,
-					}
+			if(window.innerWidth > 870){
+
+				// img_shadow on scroll
+				let boxes = gsap.utils.toArray('.img_shadow');
+
+				boxes.forEach((box, index) => {
+					let distance = index % 2 === 0 ? '-100%' : '100%';
+					let rotation = index % 2 === 0 ? '45' : '-45';
+					gsap.fromTo(box, {
+						x: distance,
+						yPercent: 50, 
+						rotation: 0,
+						opacity: 0,
+					}, {
+						x: 0,
+						yPercent: 0,
+						rotation: rotation,
+						duration: 2,
+						opacity: 1,
+						ease: 'expo.out',
+						scrollTrigger: {
+							trigger: box,
+							start: '-=200px center',
+							end: 'bottom 20%',
+							// scrub: .5,
+						}
+					})
 				})
-			})
+	
+	
+				// pc slight parallaxe 
+				let imgs = gsap.utils.toArray('.feat_container > img');
 
-
-			// pc slight parallaxe 
-			let imgs = gsap.utils.toArray('.feat_container > img');
-
-			imgs.forEach((img, index) => {
-				gsap.to(img, {
-					y: "-5%",
-					ease: "none",
-					scrollTrigger: {
-						trigger: img,
-						start: "top center",
-						scrub: 1,
-						// markers: true,
-					},
-				})
-			});
+				imgs.forEach((img, index) => {
+					gsap.to(img, {
+						y: "-5%",
+						ease: "none",
+						scrollTrigger: {
+							trigger: img,
+							start: "top center",
+							scrub: 1,
+							// markers: true,
+						},
+					})
+				});
+			} 
 
 		})
 	</script>
