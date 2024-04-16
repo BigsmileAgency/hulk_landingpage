d7eb8f67229bceecacb47c895ae5aec3fae79f3e
Jonathan Rauzy <jr@BIGSMILE.local>
2024-04-16 17:14:14 +0200

    fixing mail in english

--- web/app/themes/blank/src/404.php
+++ web/app/themes/blank/src/404.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 <main class="default">
-  <section id="top__title" class="center page__error">
-    <div class="inner">
+  <section>
+    <div class="container" style="text-align: center; margin: auto; padding: 50px 0;">
       <h1><?= __('Not found', 'bsa') ?></h1>
       <div class="content">
         <a class="cta default" href="<?= esc_url(home_url()); ?>" aria-label="<?= __('Back to home page', 'bsa'); ?>">
