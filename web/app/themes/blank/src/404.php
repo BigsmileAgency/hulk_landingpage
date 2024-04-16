a86ad85c63ed0aae97f438f19cf38c72d0e52dee
Jonathan Rauzy <jr@BIGSMILE.local>
2024-04-16 16:32:38 +0200

    404 in english + translation

--- web/app/themes/blank/src/404.php
+++ web/app/themes/blank/src/404.php
@@ -1,19 +1,16 @@
 <?php get_header(); ?>
 
 <main class="default">
-
   <section id="top__title" class="center page__error">
     <div class="inner">
-
-      <h1><?= __('Page introuvable', 'bsa') ?></h1>
+      <h1><?= __('Not found', 'bsa') ?></h1>
       <div class="content">
-        <a class="cta default" href="<?= esc_url(home_url()); ?>" aria-label="<?= __('Retour à accueil', 'bsa'); ?>">
-          <span><?= __('Retour à accueil', 'bsa'); ?></span>
+        <a class="cta default" href="<?= esc_url(home_url()); ?>" aria-label="<?= __('Back to home page', 'bsa'); ?>">
+          <span><i class="fa-solid fa-arrow-left"></i> <?= __('Back to home page', 'bsa'); ?></span>
         </a>
       </div>
     </div>
   </section>
-
 </main>
 
 <?php get_footer(); ?>
