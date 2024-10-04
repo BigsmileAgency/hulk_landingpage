e9ecc23877e25beb7ec1c1029b76888905496498
JR <jr@bigsmile.be>
2024-10-04 16:11:53 +0200

    connect with token test 3

--- web/app/themes/blank/src/components/functions/stripe/get_costumer_w_token.php
+++ web/app/themes/blank/src/components/functions/stripe/get_costumer_w_token.php
@@ -1,45 +1,48 @@
 <?php
 
 
-use Stripe\Stripe;
-use PDO;
-
-$stripe_secret_key = getenv('STRIPE_KEY');
-Stripe::setApiKey($stripe_secret_key);
-
-$platform_dbname = getenv('PLATFORM_DB_NAME');
-$platform_dbuser = getenv('PLATFORM_DB_USER');
-$platform_dbpwd = getenv('PLATFORM_DB_PWD');
+echo "TOKEN";
 
 
-$token = sanitize_text_field($_GET['t']);
+// use Stripe\Stripe;
+// use PDO;
 
-try {
-  $dsn = "mysql:host=localhost;dbname=$platform_dbname;charset=utf8mb4";
-  $pdo = new PDO($dsn, $platform_dbuser, $platform_dbpwd, [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-  ]);
+// $stripe_secret_key = getenv('STRIPE_KEY');
+// Stripe::setApiKey($stripe_secret_key);
 
-  $stmt = $pdo->prepare("SELECT stripe_id FROM users WHERE token = :token");
-  $stmt->execute([':token' => $token]);
+// $platform_dbname = getenv('PLATFORM_DB_NAME');
+// $platform_dbuser = getenv('PLATFORM_DB_USER');
+// $platform_dbpwd = getenv('PLATFORM_DB_PWD');
 
-  $result = $stmt->fetch();
 
-  if ($result) {
-    $stripe_customer_id = $result['stripe_id'];
+// $token = sanitize_text_field($_GET['t']);
 
-    $customer = \Stripe\Customer::retrieve($stripe_customer_id);
+// try {
+//   $dsn = "mysql:host=localhost;dbname=$platform_dbname;charset=utf8mb4";
+//   $pdo = new PDO($dsn, $platform_dbuser, $platform_dbpwd, [
+//     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//   ]);
 
-    if ($customer) {
-      $costumer = $customer;
-    }
-  } else {
-    // echo "<script>window.location.href='/404';</script>";
-    exit;
-  }
-} catch (Exception $e) {
-  error_log("Erreur lors de la connexion à la base de données ou récupération du client : " . $e->getMessage());
-  // echo "<script>window.location.href='/404';</script>";
-  exit;
-}
+//   $stmt = $pdo->prepare("SELECT stripe_id FROM users WHERE token = :token");
+//   $stmt->execute([':token' => $token]);
+
+//   $result = $stmt->fetch();
+
+//   if ($result) {
+//     $stripe_customer_id = $result['stripe_id'];
+
+//     $customer = \Stripe\Customer::retrieve($stripe_customer_id);
+
+//     if ($customer) {
+//       $costumer = $customer;
+//     }
+//   } else {
+//     // echo "<script>window.location.href='/404';</script>";
+//     exit;
+//   }
+// } catch (Exception $e) {
+//   error_log("Erreur lors de la connexion à la base de données ou récupération du client : " . $e->getMessage());
+//   // echo "<script>window.location.href='/404';</script>";
+//   exit;
+// }
