a38dc2588017490d17a5a6876366a8be23febd05
JR <jr@bigsmile.be>
2024-10-04 12:21:30 +0200

    test connection on staging 2

--- web/app/themes/blank/src/components/functions/stripe/create_customer_trial.php
+++ web/app/themes/blank/src/components/functions/stripe/create_customer_trial.php
@@ -23,53 +23,46 @@ function create_customer_trial()
 
   $user_id = uniqid();
 
-  $insert = insert_customer_to_platform($user_id, $firstname, $lastname, $company, $email, $pwd);
-  wp_send_json_success([
-    'message' => "Trial has started now",
-    'insert' => $insert
-  ]);
+  try {
+    $customer = \Stripe\Customer::create([
+      'name' => $firstname . " " . $lastname,
+      'email' => $email,
+      'metadata' => [
+        'firstname' => $firstname,
+        'lastname' => $lastname,
+        'company' => $company,
+        'tel' => $tel,
+        'tva' => $tva,
+        'user_id' => $user_id,
+      ],
+      'address' => [
+        'line1' => $address,
+        'postal_code' => $zip,
+      ],
+    ]);
 
+    $subscription = \Stripe\Subscription::create([
+      'customer' => $customer->id,
+      'trial_period_days' => 8,
+      'items' => [['price' => 'price_1Q5Rcn2KTIC8Xb8EJGv7x1GS']], // basic sub is large monthly for trial
+      'expand' => ['latest_invoice.payment_intent'],
+    ]);
 
-  // try {
-  //   $customer = \Stripe\Customer::create([
-  //     'name' => $firstname . " " . $lastname,
-  //     'email' => $email,
-  //     'metadata' => [
-  //       'firstname' => $firstname,
-  //       'lastname' => $lastname,
-  //       'company' => $company,
-  //       'tel' => $tel,
-  //       'tva' => $tva,
-  //       'user_id' => $user_id,
-  //     ],
-  //     'address' => [
-  //       'line1' => $address,
-  //       'postal_code' => $zip,
-  //     ],
-  //   ]);
+    if ($subscription->status === 'trialing') {
 
-  //   $subscription = \Stripe\Subscription::create([
-  //     'customer' => $customer->id,
-  //     'trial_period_days' => 8,
-  //     'items' => [['price' => 'price_1Q5Rcn2KTIC8Xb8EJGv7x1GS']], // basic sub is large monthly for trial
-  //     'expand' => ['latest_invoice.payment_intent'],
-  //   ]);
+      $insert = insert_customer_to_platform($user_id, $firstname, $lastname, $company, $email, $pwd);
+      wp_send_json_success([
+        'message' => "Trial has started now",
+        'insert' => $insert['message']
+      ]);
+    } else {
 
-  //   if ($subscription->status === 'trialing') {
+      wp_send_json_error(['message' => "Erreur lors de la création de l'abonnement."]);
+    }
+  } catch (Exception $e) {
 
-  //     $insert = insert_customer_to_platform($user_id, $firstname, $lastname, $company, $email, $pwd);
-  //     wp_send_json_success([
-  //       'message' => "Trial has started now",
-  //       'insert' => $insert['message']
-  //     ]);
-  //   } else {
-
-  //     wp_send_json_error(['message' => "Erreur lors de la création de l'abonnement."]);
-  //   }
-  // } catch (Exception $e) {
-
-  //   wp_send_json_error(['message' => $e->getMessage()]);
-  // }
+    wp_send_json_error(['message' => $e->getMessage()]);
+  }
 }
 
 function insert_customer_to_platform($user_id, $firstname, $lastname, $company, $email, $pwd)
@@ -85,21 +78,21 @@ function insert_customer_to_platform($user_id, $firstname, $lastname, $company, 
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]);
 
-    // $stmt = $pdo->prepare(
-    //   "INSERT INTO users (user_id, firstname, lastname, company, email, password, isAdmin, ownerID) 
-    //   VALUES (:user_id, :firstname, :lastname,  :company, :email, :password, :isAdmin, :ownerID)"
-    // );
+    $stmt = $pdo->prepare(
+      "INSERT INTO users (user_id, firstname, lastname, company, email, password, isAdmin, ownerID) 
+      VALUES (:user_id, :firstname, :lastname,  :company, :email, :password, :isAdmin, :ownerID)"
+    );
 
-    // $stmt->execute([
-    //   ':user_id' => $user_id,
-    //   ':firstname' => $firstname,
-    //   ':lastname' => $lastname,
-    //   ':company' => $company,
-    //   ':email' => $email,
-    //   ':password' => $pwd,
-    //   ':isAdmin' => '0',
-    //   ':ownerID' => '0',
-    // ]);
+    $stmt->execute([
+      ':user_id' => $user_id,
+      ':firstname' => $firstname,
+      ':lastname' => $lastname,
+      ':company' => $company,
+      ':email' => $email,
+      ':password' => $pwd,
+      ':isAdmin' => '0',
+      ':ownerID' => '0',
+    ]);
 
     return ['success' => true, 'message' => 'Insertion dans la plateforme réussie.'];
   } catch (Exception $e) {
