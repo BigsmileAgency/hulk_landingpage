1eac0e60a8dcc855e11fe12cfb25529e4afe9890
JR <jr@bigsmile.be>
2024-10-04 16:03:32 +0200

    connect with token test 1

--- web/app/themes/blank/src/page-sign-up_confirm.php
+++ web/app/themes/blank/src/page-sign-up_confirm.php
@@ -5,19 +5,28 @@
  */
 ?>
 
-<?php 
+<?php
 
-get_header(); 
+get_header();
 
-if(!empty($_GET)){
-  $session_id = $_GET['session_id'];
-  var_dump($session_id);
-  if(isset($session_id)){
-    $session = \Stripe\Checkout\Session::retrieve($session_id);
-    $customer = \Stripe\Customer::retrieve($session->customer);    
-    var_dump($customer); 
-  }
-}
+if(!empty($_GET['t'])){
+
+  include_once get_template_directory() . '/components/functions/stripe/get_costumer_w_token.php';
+  $costumer = true;
+
+} else { 
+
+?>
+
+<script>
+  window.location.href ='/404';
+</script>
+
+<?php
+} 
+
+
+if(isset($costumer)):
 
 ?>
 
@@ -36,30 +45,6 @@ if(!empty($_GET)){
               <h3><?= __('Update', 'hulkbanner') ?></h3>
             </div>
             <form id="first-part-form" action="" method="post">
-              <label for="firstname"><?= __('Firstname', 'hulkbanner') ?></label>
-              <input type="text" name="firstname" id="firstname">
-              <br>
-              <label for="lastname"><?= __('Lastname', 'hulkbanner') ?></label>
-              <input type="text" name="lastname" id="lastname">
-              <br>
-              <label for="email"><?= __('Eail', 'hulkbanner') ?></label>
-              <input type="email" name="email" id="email">
-              <br>
-              <label for="tel"><?= __('Phone', 'hulkbanner') ?></label>
-              <input type="text" name="tel" id="tel">
-              <br>
-              <label for="address"><?= __('Address', 'hulkbanner') ?></label>
-              <input type="text" name="address" id="address">
-              <br>
-              <label for="zipcode"><?= __('Zipcode', 'hulkbanner') ?></label>
-              <input type="text" name="zipcode" id="zipcode">
-              <br>
-              <label for="company"><?= __('Company', 'hulkbanner') ?></label>
-              <input type="text" name="company" id="company">
-              <br>
-              <label for="tva"><?= __('TVA number', 'hulkbanner') ?></label>
-              <input type="text" name="tva" id="tva">
-              <br>
               <div class="billing_plan">
                 <div class="billing_choice">
                   <label for="monthly"><?= __('Monthly', 'hulkbanner') ?></label>
@@ -92,12 +77,6 @@ if(!empty($_GET)){
                 <!-- STRIPE ELEMENTS GOES HERE -->
               </div>
               <input type="submit" value="<?= __('Continue', 'hulkbanner') ?>">
-              <div class="divider">
-                <hr>
-                <p> or </p>
-                <hr>
-              </div>
-              <div class="google_login">Google signup</div>
             </form>
           </div>
         </div>
@@ -116,7 +95,6 @@ if(!empty($_GET)){
     const elements = stripe.elements();
     const cardElement = elements.create('card');
     cardElement.mount('#card_element');
-
 
     // Sélectionner tous les inputs radio avec le nom "billing"
     let billingInputs = document.querySelectorAll('input[name="billing"]');
@@ -141,11 +119,10 @@ if(!empty($_GET)){
           });
         }
 
-        // Réinitialiser la sélection du dropdown
         let dropdown = document.getElementById('subscription_type');
         if (dropdown) {
           dropdown.selectedIndex = 0;
-        } 
+        }
       });
     });
 
@@ -153,77 +130,17 @@ if(!empty($_GET)){
     // SUBMIT 
     document.getElementById('first-part-form').addEventListener('submit', async (event) => {
 
-      // A RETIRER
       event.preventDefault();
 
-      // Récupérer les valeurs des champs
-      let firstname = document.getElementById('firstname').value;
-      let lastname = document.getElementById('lastname').value;
-      let email = document.getElementById('email').value;
-      let address = document.getElementById('address').value;
-      let zip = document.getElementById('zipcode').value
-      let tel = document.getElementById('tel').value;
-      let company = document.getElementById('company').value;
-      let tva = document.getElementById('tva').value;
       let selectedBilling = document.querySelector('input[name="billing"]:checked').value;
       let selectedPlan = document.getElementById('subscription_type').value;
 
-
-      if (firstname === "" || lastname === "" || email === "" || tel === "" || company === "" || tva === "" || selectedBilling === "" || selectedPlan === "") {
-        event.preventDefault();
+      if (selectedBilling === "" || selectedPlan === "") {
         alert('All the fields are mandatory exept "Logo"');
       } else {
-        const {
-          error,
-          paymentMethod
-        } = await stripe.createPaymentMethod({
-          type: 'card',
-          card: cardElement,
-          billing_details: {
-            name: company,
-            email,
-            address: {
-              line1: address,
-              postal_code: zip
-            }
-          }
-        });
 
-        if (error) {
-          alert(error.message);
-        } else {
-          let url = '<?= admin_url('admin-ajax.php') ?>';
+        // REQUETE AJAX ICI
 
-          let formData = new URLSearchParams({
-            action: 'stripe_handler',
-            paymentMethod: paymentMethod.id,
-            firstname,
-            lastname,
-            company,
-            email,
-            address,
-            zip,
-            tel,
-            tva,
-            selectedBilling,
-            selectedPlan
-          })
-
-          fetch(url, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: formData.toString()
-          }).then(response => response.json()).then(data => {
-            console.log(data);
-            if (data.success) {
-              alert(data.message);
-            } else {
-              alert(data.message);
-            }
-          })
-        }
       }
     });
 
@@ -234,5 +151,16 @@ if(!empty($_GET)){
     }
   });
 </script>
+
+
+<?php else: ?>
+
+
+<script>
+  window.location.href ='/404';
+</script>
+
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
