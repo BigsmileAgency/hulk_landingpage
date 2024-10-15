73f04df229da10c89d9c8d7920df9e483852ce2e
dylan <dylan @bigsmile.be>
2024-10-15 15:16:24 +0200

    Update Front

--- web/app/themes/blank/src/components/functions/lang_switch_handler.php
+++ web/app/themes/blank/src/components/functions/lang_switch_handler.php
@@ -14,51 +14,6 @@ function lang_switch_handler()
 			if (langArray.includes(lang)) {
 				otherLang = langArray.filter((e) => e !== lang);
 			}
-
-			function displayButtons() {
-				// Selon la taille de l'écran 
-				if (window.innerWidth > 768) {
-					switchContainer.innerHTML = `<a class="current_lang lang_display">${lang.toUpperCase()}</a>`
-					otherLang.map((e) => {
-						switchContainer.innerHTML += `<a href="/${e}${window.location.pathname.replace('/' + lang, '')}" class="other_lang lang_display">${e.toUpperCase()}</a>`
-					})
-				} else {
-					mobileSwitch.innerHTML = `<a class="current_lang lang_display">${lang.toUpperCase()}</a>`
-					otherLang.map((e) => {
-						// mobileSwitch.innerHTML += `<a class="other_lang lang_display">${e.toUpperCase()}</a>`
-						mobileSwitch.innerHTML += `<a href="/${e}${window.location.pathname.replace('/' + lang, '')}" class="other_lang lang_display">${e.toUpperCase()}</a>`
-
-					})
-				}
-				 
-				// let langBtn = document.querySelectorAll('.lang_display');
-				// langBtn.forEach((element) => {
-				// 	element.addEventListener('click', ({target}) => {
-				// 		if (target.innerHTML.toLowerCase() !== lang) {
-				// 			let newLang = target.innerHTML.toLowerCase();
-				// 			if (newLang == "en") {
-				// 				window.location = window.location.href.replace('/' + lang, '');		
-				// 			} else {
-				// 				window.location = '/' + newLang + window.location.pathname.replace('/' + lang, '');
-				// 			}
-				// 		}
-				// 	});
-				// 	element.addEventListener("keyup", ({keyCode,target}) => {
-				// 		if (keyCode == 13) {
-				// 			if (target.innerHTML.toLowerCase() !== lang) {
-				// 				let newLang = target.innerHTML.toLowerCase();
-				// 				if (newLang == "en") {
-				// 					window.location = window.location.href.replace('/' + lang, '');		
-				// 				} else {
-				// 					window.location = '/' + newLang + window.location.pathname.replace('/' + lang, '');
-				// 				}
-				// 			}
-				// 		}
-				// 	})
-				// });
-			}
-			// displayButtons();
-			window.addEventListener('resize', displayButtons);
 		})
 	</script>
 <?php
